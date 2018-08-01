8255a73cd3664389e455c32f64edfc7c48ee1f2d
Mario Borras <mario.borras@icloud.com>
2018-08-01 16:28:06 -0400

    Added modal to the site
    Added modal to the site

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+setTimeout(function() {
+    document.querySelector("#modal").style ="display: block"
+}, 3000)
+
+    function onclickx() {
+        document.getElementById("modal").style ="display: none"
+    }
+    
