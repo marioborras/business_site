df35bd0e40a0ebf4cfa466136efa8b8fef4c4cac
Mario Borras <mario.borras@icloud.com>
2018-08-01 14:50:41 -0400

    The modal is done!
    The modal is done!

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+setTimeout(function() {
+    document.querySelector("#modal").style ="display: block"}, 3000)
+
+    function onclickx() {
+        document.getElementById("modal").style ="display: none"}
+    
